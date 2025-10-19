7e4ab3012c2766e51fc86fbe19dd812b85599d39
Mukesh Yogi <mukeshyogi7766@gmail.com>
2025-10-19 13:13:31 +0000

    aesa site map banao (This XML file does not appear to have any style inf

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,3 +1,4 @@
+
 import type { MetadataRoute } from 'next'
 import { getFestivalsWithTargetDate } from '@/lib/festivals';
  
@@ -6,15 +7,36 @@ export default function sitemap(): MetadataRoute.Sitemap {
   const siteUrl = "https://festivalcountdown.netlify.app/";
 
   const festivalUrls = allFestivalsWithDates.map((festival) => ({
-    url: `${siteUrl}/festivals/${festival.slug}`,
+    url: `${siteUrl}festivals/${festival.slug}`,
     lastModified: new Date(),
+    changeFrequency: 'daily' as 'daily',
   }));
- 
-  return [
+
+  const staticUrls = [
     {
       url: siteUrl,
       lastModified: new Date(),
+      changeFrequency: 'daily' as 'daily',
     },
+    {
+      url: `${siteUrl}about`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly' as 'monthly',
+    },
+    {
+        url: `${siteUrl}privacy-policy`,
+        lastModified: new Date(),
+        changeFrequency: 'monthly' as 'monthly',
+    },
+    {
+        url: `${siteUrl}terms-and-conditions`,
+        lastModified: new Date(),
+        changeFrequency: 'monthly' as 'monthly',
+    }
+  ];
+ 
+  return [
+    ...staticUrls,
     ...festivalUrls
   ]
 }
