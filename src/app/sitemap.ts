deb094d130a7aae842687324e81b2673d5bc31fe
Mukesh Yogi <mukeshyogi7766@gmail.com>
2025-10-21 14:14:46 +0000

    Sitemap could not be read (google search consol me yha erroe show kar rh

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,28 +5,34 @@ import { getFestivalsWithTargetDate } from '@/lib/festivals';
 export default function sitemap(): MetadataRoute.Sitemap {
   const { allFestivalsWithDates } = getFestivalsWithTargetDate();
   const siteUrl = "https://festivalcountdown.netlify.app/";
+  const now = new Date();
 
   const festivalUrls = allFestivalsWithDates.map((festival) => ({
     url: `${siteUrl}festivals/${festival.slug}`,
+    lastModified: now,
     changeFrequency: 'daily' as 'daily',
   }));
 
   const staticUrls = [
     {
       url: siteUrl,
+      lastModified: now,
       changeFrequency: 'daily' as 'daily',
     },
     {
       url: `${siteUrl}about`,
-      changeFrequency: 'daily' as 'daily',
+      lastModified: now,
+      changeFrequency: 'monthly' as 'monthly',
     },
     {
         url: `${siteUrl}privacy-policy`,
-        changeFrequency: 'daily' as 'daily',
+        lastModified: now,
+        changeFrequency: 'monthly' as 'monthly',
     },
     {
         url: `${siteUrl}terms-and-conditions`,
-        changeFrequency: 'daily' as 'daily',
+        lastModified: now,
+        changeFrequency: 'monthly' as 'monthly',
     }
   ];
  
